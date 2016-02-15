31a752639e74d6d8b7c2945def521f45fdcdf964
Alexander Kapitonenko <apiton2008@rambler.ru>
2016-02-15 14:47:55 +0300

    duplication deleted in tests

--- src/test/java/AnotherScoreCalculateTest.java
+++ src/test/java/AnotherScoreCalculateTest.java
@@ -1,4 +1,11 @@
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theory;
+import sun.util.locale.LocaleUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -6,34 +13,22 @@ import static org.junit.Assert.*;
  * Created by kapiton on 13.02.16.
  */
 public class AnotherScoreCalculateTest {
-    @Test
-    public void testAllZero() {
-        String[] testHits = new String[]{"00","00","00","00","00","00","00","00","00","00"};
-        assertEquals(0, AnotherScoreCalculate.calculateScore(testHits));
+
+    public static final Map<String[], Integer> testData = new HashMap<String[], Integer>();
+
+    @Before
+    public void setUp() {
+        testData.put(new String[]{"00","00","00","00","00","00","00","00","00","00"}, 0);
+        testData.put(new String[]{"12","42","15","72","12","32","12","15","00","23"}, 46 );
+        testData.put(new String[]{"x","x","x","x","x","x","x","x","x","xxx"}, 300 );
+        testData.put(new String[]{"1/","4/","1/","7/","1/","3/","1/","1/","0/","2/3"}, 123 );
+        testData.put(new String[]{"x","13","12","1/","x","12","12","17","12","x3x"}, 94 );
     }
 
     @Test
-    public void testAllSimpleZero() {
-        String[] testHits = new String[]{"12","42","15","72","12","32","12","15","00","23"};
-        assertEquals(46, AnotherScoreCalculate.calculateScore(testHits));
-    }
-
-    @Test
-    public void testAllStrike() {
-        String[] testHits = new String[]{"x","x","x","x","x","x","x","x","x","xxx"};
-        assertEquals(300, AnotherScoreCalculate.calculateScore(testHits));
-    }
-
-    @Test
-    public void testAllSpair() {
-        String[] testHits = new String[]{"1/","4/","1/","7/","1/","3/","1/","1/","0/","2/3"};
-        assertEquals(123, AnotherScoreCalculate.calculateScore(testHits));
-    }
-
-    @Test
-    public void testAll() {
-        String[] testHits = new String[]{"x","13","12","1/","x","12","12","17","12","x3x"};
-        assertEquals(94, AnotherScoreCalculate.calculateScore(testHits));
+    public void testCalculate() {
+        for( HashMap.Entry<String[], Integer> entry: testData.entrySet())
+            assertEquals(entry.getValue().intValue(), AnotherScoreCalculate.calculateScore(entry.getKey()));
     }
 
 
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,14 +1,31 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.BufferedReader;
+import java.util.HashMap;
+import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 /**
  * Created by kapiton on 02.02.16.
  */
 public class MainTest {
+
+    public static final Map<String, Boolean> testData = new HashMap<String, Boolean>();
+
+    @Before
+    public void setUp() {
+        testData.put("23", true);
+        testData.put("65", false );
+        testData.put("265", false );
+        testData.put("", false );
+        testData.put("65/", false );
+        testData.put("x2", false );
+        testData.put("/2", false );
+    }
 
     @Test
     public void testReadFromBuffer() throws Exception {
@@ -20,29 +37,20 @@ public class MainTest {
 
     @Test
     public void testCheckFrame() throws Exception {
-        Assert.assertEquals("23", true, Main.checkFrame("23"));
+        for( HashMap.Entry<String, Boolean> entry: testData.entrySet())
+            assertEquals(entry.getKey(), entry.getValue(), Main.checkFrame(entry.getKey()));
+
         Assert.assertEquals("x", true, Main.checkFrame("x"));
         Assert.assertEquals("2/", true, Main.checkFrame("2/"));
 
-        Assert.assertEquals("65", false, Main.checkFrame("65"));
-        Assert.assertEquals("", false, Main.checkFrame(""));
-        Assert.assertEquals("652", false, Main.checkFrame("652"));
-        Assert.assertEquals("65/", false, Main.checkFrame("65/"));
-        Assert.assertEquals("x2", false, Main.checkFrame("x2"));
-        Assert.assertEquals("/2", false, Main.checkFrame("/2"));
     }
 
     @Test
     public void testCheckLastFrame() throws Exception {
-        Assert.assertEquals("23", true, Main.checkLastFrame("23"));
+        for( HashMap.Entry<String, Boolean> entry: testData.entrySet())
+            assertEquals(entry.getKey(), entry.getValue(), Main.checkFrame(entry.getKey()));
+
         Assert.assertEquals("x32", true, Main.checkLastFrame("x32"));
         Assert.assertEquals("2/4", true, Main.checkLastFrame("2/4"));
-
-        Assert.assertEquals("265", false, Main.checkLastFrame("265"));
-        Assert.assertEquals("", false, Main.checkLastFrame(""));
-        Assert.assertEquals("56", false, Main.checkLastFrame("56"));
-        Assert.assertEquals("65/", false, Main.checkLastFrame("65/"));
-        Assert.assertEquals("x2", false, Main.checkLastFrame("x2"));
-        Assert.assertEquals("/2", false, Main.checkLastFrame("/2"));
     }
 }
