637bff66b6eae0706274e1f210517be5eefb9fed
Alexander Kapitonenko <apiton2008@rambler.ru>
2016-02-15 14:59:09 +0300

    some tests fixed

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,12 +1,16 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.Theory;
 
 import java.io.BufferedReader;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeThat;
 import static org.mockito.Mockito.*;
 
 /**
@@ -15,6 +19,8 @@ import static org.mockito.Mockito.*;
 public class MainTest {
 
     public static final Map<String, Boolean> testData = new HashMap<String, Boolean>();
+    public static final Map<String, Boolean> testDataForCheckFrame = new HashMap<String, Boolean>();
+    public static final Map<String, Boolean> testDataForCheckLastFrame = new HashMap<String, Boolean>();
 
     @Before
     public void setUp() {
@@ -25,6 +31,12 @@ public class MainTest {
         testData.put("65/", false );
         testData.put("x2", false );
         testData.put("/2", false );
+
+        testDataForCheckFrame.put("x", true);
+        testDataForCheckFrame.put("2/", true);
+
+        testDataForCheckLastFrame.put("x32", true);
+        testDataForCheckLastFrame.put("2/4", true);
     }
 
     @Test
@@ -40,17 +52,17 @@ public class MainTest {
         for( HashMap.Entry<String, Boolean> entry: testData.entrySet())
             assertEquals(entry.getKey(), entry.getValue(), Main.checkFrame(entry.getKey()));
 
-        Assert.assertEquals("x", true, Main.checkFrame("x"));
-        Assert.assertEquals("2/", true, Main.checkFrame("2/"));
+        for( HashMap.Entry<String, Boolean> entry: testDataForCheckFrame.entrySet())
+            assertEquals(entry.getKey(), entry.getValue(), Main.checkFrame(entry.getKey()));
 
     }
 
     @Test
     public void testCheckLastFrame() throws Exception {
         for( HashMap.Entry<String, Boolean> entry: testData.entrySet())
-            assertEquals(entry.getKey(), entry.getValue(), Main.checkFrame(entry.getKey()));
+            assertEquals(entry.getKey(), entry.getValue(), Main.checkLastFrame(entry.getKey()));
 
-        Assert.assertEquals("x32", true, Main.checkLastFrame("x32"));
-        Assert.assertEquals("2/4", true, Main.checkLastFrame("2/4"));
+        for( HashMap.Entry<String, Boolean> entry: testDataForCheckLastFrame.entrySet())
+            assertEquals(entry.getKey(), entry.getValue(), Main.checkLastFrame(entry.getKey()));
     }
 }
